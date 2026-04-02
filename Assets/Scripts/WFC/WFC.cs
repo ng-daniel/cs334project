fb73d86dc0e298903da191f9bc078b83599f7ecc
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2026-04-02 17:12:21 -0400

    Pasting cubes in a grid

---
+++ Assets/Scripts/WFC/WFC.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace WFC
+{
+    public class WFC : MonoBehaviour
+    {
+        public GameObject cube;
+
+        public float tileScale = 1;
+        public float tileHeight = 0.2f;
+
+        void Start()
+        {
+            PlaceCube(0, 0);
+            PlaceCube(1, 0);
+            PlaceCube(2, 0);
+            PlaceCube(3, 0);
+            PlaceCube(4, 0);
+            PlaceCube(2, 1);
+        }
+
+        void Update()
+        {
+
+        }
+
+        private void PlaceCube(int x, int y)
+        {
+            GameObject go = GameObject.Instantiate(cube);
+            go.transform.position = new Vector3(x * tileScale, 0, y * tileScale);
+            go.transform.localScale = new Vector3(tileScale, tileHeight, tileScale);
+        }
+    }
+}
